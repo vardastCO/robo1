85a9c9243bbc793e80cd90a2b1f42eda76ddfd72
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-09-29 22:58:26 +0330

    v

--- scrape.js
+++ scrape.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const csvWriter = createCsvWriter({
