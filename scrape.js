eec9f5ac69e1f48cec97b559f231bd04a0cf9e5e
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-10-03 14:56:09 +0330

    c

--- scrape.js
+++ scrape.js
@@ -15,22 +15,22 @@ const startUrlPattern = 'https://www.hypersaz.com/product.php?';
 
 (async () => {
     let browser; // Declare the browser variable
-    const proxyServer =
-        'ss://YWVzLTI1Ni1nY206d0DVaGt6WGpjRA==@38.54.13.15:31214#main';
-    try {
-        browser = await puppeteer.launch({
-            headless: true, // Set to true for headless mode, false for non-headless
-            executablePath: '/usr/bin/google-chrome',
-            args: [
-                '--no-sandbox',
-                `--proxy-server=${proxyServer}`,
-                '--disable-setuid-sandbox',
-            ],
-        });
-    } catch (error) {
-        console.error('Error while launching the browser:', error);
-        return;
-    }
+        const proxyServer =
+            'ss://YWVzLTI1Ni1nY206d0DVaGt6WGpjRA==@38.54.13.15:31214#main';
+        try {
+            browser = await puppeteer.launch({
+                headless: "true", // Set to true for headless mode, false for non-headless
+                executablePath: '/usr/bin/google-chrome',
+                args: [
+                    '--no-sandbox',
+                    // `--proxy-server=${proxyServer}`,
+                    '--disable-setuid-sandbox',
+                ],
+            });
+        } catch (error) {
+            console.error('Error while launching the browser:', error);
+        }
+
 
     const processedHrefs = new Set();
     const unprocessedHrefs = new Set();
@@ -38,43 +38,40 @@ const startUrlPattern = 'https://www.hypersaz.com/product.php?';
     async function processPage(pageUrl) {
         console.log('Processing Page:', pageUrl);
         const page = await browser.newPage();
+        await page.goto(pageUrl, { timeout: 30000 });
 
-        try {
-            await page.goto(pageUrl, { timeout: 60000 }); // Increased timeout to 60 seconds
-
-            const priceElement = await page.$x(
-                '/html/body/section[2]/div/div/div[3]/div/ul/li[2]/p/span'
+        const priceElement = await page.$x(
+            '/html/body/section[2]/div/div/div[3]/div/ul/li[2]/p/span'
+        );
+        if (priceElement.length > 0) {
+            const priceText = await page.evaluate(
+                (el) => el.textContent,
+                priceElement[0]
             );
-            if (priceElement.length > 0) {
-                const priceText = await page.evaluate(
-                    (el) => el.textContent,
-                    priceElement[0]
-                );
-                if (priceText.trim() !== '') {
-                    const record = [{ url: pageUrl, price: priceText.trim() }];
-                    await csvWriter.writeRecords(record);
-                    console.log(`Saved: URL: ${pageUrl}, Price: ${priceText.trim()}`);
-                }
+            if (priceText.trim() !== '') {
+                const record = [{ url: pageUrl, price: priceText.trim() }];
+                await csvWriter.writeRecords(record);
+                console.log(`Saved: URL: ${pageUrl}, Price: ${priceText.trim()}`);
             }
-
-            const hrefs = await page.evaluate(() => {
-                const links = Array.from(document.querySelectorAll('a'));
-                return links.map((link) => link.getAttribute('href'));
-            });
-
-            for (const href of hrefs) {
-                if (href && !processedHrefs.has(href)) {
-                    const outputUrl = href.startsWith('https://') ? href : initialPage + href;
-                    if (outputUrl.startsWith(startUrlPattern)) {
-                        unprocessedHrefs.add(outputUrl);
-                    }
-                }
-            }
-        } catch (error) {
-            console.error('Error processing page:', error);
-        } finally {
-            await page.close();
         }
+        const hrefs = await page.evaluate(() => {
+            const links = Array.from(document.querySelectorAll('a'));
+            return links.map((link) => link.getAttribute('href'));
+        });
+
+        for (const href of hrefs) {
+            if (href && !processedHrefs.has(href)) {
+                if (!href.startsWith('https://')) {
+                    var outputUrl = initialPage + href;
+                } else {
+                    var outputUrl = href;
+                }
+                if (outputUrl.startsWith(startUrlPattern)) {
+                    unprocessedHrefs.add(outputUrl);
+                }
+            }
+        }
+        await page.close();
     }
 
     try {
@@ -88,11 +85,11 @@ const startUrlPattern = 'https://www.hypersaz.com/product.php?';
             const pageForEvaluation = await browser.newPage();
 
             let retryCount = 0;
-            const maxRetries = 10; // Define the maximum number of retries
+            const maxRetries = 10000; // Define the maximum number of retries
 
             while (retryCount < maxRetries) {
                 try {
-                    await processPage(currentHref);
+                    await processPage(currentHref); // Increase the timeout to 30 seconds
                     break; // If successful, break out of the loop
                 } catch (error) {
                     if (error.name === 'TimeoutError') {
@@ -100,7 +97,6 @@ const startUrlPattern = 'https://www.hypersaz.com/product.php?';
                             `Timeout occurred (Retry ${retryCount + 1}/${maxRetries}). Retrying...`
                         );
                         retryCount++;
-                        await pageForEvaluation.waitForTimeout(3000); // Wait for 3 seconds between retries
                     } else {
                         throw error; // Rethrow other errors
                     }
@@ -112,6 +108,8 @@ const startUrlPattern = 'https://www.hypersaz.com/product.php?';
                     `Max retries (${maxRetries}) reached. Unable to load the page: ${currentHref}`
                 );
                 // Handle the situation when the page can't be loaded after maximum retries.
+                await pageForEvaluation.close();
+                continue; // Move on to the next URL
             }
 
             await pageForEvaluation.close();
