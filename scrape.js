b01d151d45661e4b932760039a0276ba06b8c9b0
partovi <partovi74@gmail.com>
2023-10-05 07:12:40 -0700

    cahnge seed

--- scrape.js
+++ scrape.js
@@ -40,107 +40,116 @@ const initialPage = 'https://www.hypersaz.com/';
 const startUrlPattern = 'https://www.hypersaz.com/product.php?';
 
 async function main() {
-    let browser;
+    
     const processedHrefs = new Set();
     const unprocessedHrefs = new Set();
-
-    async function processPage(pageUrl) {
-        console.log('Processing Page:', pageUrl);
-        try {
-            console.log(browser,'broooooowser')
-            const page = await browser.newPage();
-            await page.goto(pageUrl, { timeout: 120000 });
-            await page.screenshot();
-            const priceElement = await page.$x(
-                '/html/body/section[2]/div/div/div[3]/div/ul/li[2]/p/span'
-            );
-            if (priceElement.length > 0) {
-                const priceText = await page.evaluate(
-                    (el) => el.textContent,
-                    priceElement[0]
+    if(await browser){
+        async function processPage(pageUrl) {
+            console.log('Processing Page:', pageUrl);
+            try {
+                console.log(browser,'broooooowser')
+                const page = await browser.newPage();
+                await page.goto(pageUrl, { timeout: 120000 });
+                await page.screenshot();
+                const priceElement = await page.$x(
+                    '/html/body/section[2]/div/div/div[3]/div/ul/li[2]/p/span'
                 );
-                if (priceText.trim() !== '') {
-                    const record = [{ url: pageUrl, price: priceText.trim() }];
-                    await csvWriter.writeRecords(record);
-                    console.log(`Saved: URL: ${pageUrl}, Price: ${priceText.trim()}`);
-                }
-            }
-            const hrefs = await page.evaluate(() => {
-                const links = Array.from(document.querySelectorAll('a'));
-                return links.map((link) => link.getAttribute('href'));
-            });
-    
-            for (const href of hrefs) {
-                if (href && !processedHrefs.has(href)) {
-                    if (!href.startsWith('https://')) {
-                        var outputUrl = initialPage + href;
-                    } else {
-                        var outputUrl = href;
-                    }
-                    if (outputUrl.startsWith(startUrlPattern)) {
-                        unprocessedHrefs.add(outputUrl);
+                if (priceElement.length > 0) {
+                    const priceText = await page.evaluate(
+                        (el) => el.textContent,
+                        priceElement[0]
+                    );
+                    if (priceText.trim() !== '') {
+                        const record = [{ url: pageUrl, price: priceText.trim() }];
+                        await csvWriter.writeRecords(record);
+                        console.log(`Saved: URL: ${pageUrl}, Price: ${priceText.trim()}`);
                     }
                 }
+                const hrefs = await page.evaluate(() => {
+                    const links = Array.from(document.querySelectorAll('a'));
+                    return links.map((link) => link.getAttribute('href'));
+                });
+        
+                for (const href of hrefs) {
+                    if (href && !processedHrefs.has(href)) {
+                        if (!href.startsWith('https://')) {
+                            var outputUrl = initialPage + href;
+                        } else {
+                            var outputUrl = href;
+                        }
+                        if (outputUrl.startsWith(startUrlPattern)) {
+                            unprocessedHrefs.add(outputUrl);
+                        }
+                    }
+                }
+                await page.close();
+            } catch (error) {
+                console.error('An error occurred while navigating to the page farbooood:', error);
+                // Handle the error as needed
             }
-            await page.close();
-          } catch (error) {
-            console.error('An error occurred while navigating to the page farbooood:', error);
-            // Handle the error as needed
-          }
+
+            
+        }
+
+        try {
+            unprocessedHrefs.add(initialPage);
+
+            while (unprocessedHrefs.size > 0) {
+                const currentHref = Array.from(unprocessedHrefs)[0];
+                unprocessedHrefs.delete(currentHref);
+                processedHrefs.add(currentHref);
+
+                const pageForEvaluation = await browser.newPage();
+
+                let retryCount = 0;
+                const maxRetries = 10000; // Define the maximum number of retries
+
+                while (retryCount < maxRetries) {
+                    try {
+                        await processPage(currentHref); // Increase the timeout to 30 seconds
+                        break; // If successful, break out of the loop
+                    } catch (error) {
+                        if (error.name === 'TimeoutError') {
+                            console.error(
+                                `Timeout occurred (Retry ${retryCount + 1}/${maxRetries}). Retrying...`
+                            );
+                            retryCount++;
+                        } else {
+                            throw error; // Rethrow other errors
+                        }
+                    }
+                }
+
+                if (retryCount >= maxRetries) {
+                    console.error(
+                        `Max retries (${maxRetries}) reached. Unable to load the page: ${currentHref}`
+                    );
+                    // Handle the situation when the page can't be loaded after maximum retries.
+                    await pageForEvaluation.close();
+                    continue; // Move on to the next URL
+                }
+
+                await pageForEvaluation.close();
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+        } finally {
+            if (browser) {
+                await browser.close(); // Close the browser when done
+            }
+            console.log('final')
+        }
+    }else {
+        console.log('no broser')
+    }
 
         
-    }
-
-    try {
-        unprocessedHrefs.add(initialPage);
-
-        while (unprocessedHrefs.size > 0) {
-            const currentHref = Array.from(unprocessedHrefs)[0];
-            unprocessedHrefs.delete(currentHref);
-            processedHrefs.add(currentHref);
-
-            const pageForEvaluation = await browser.newPage();
-
-            let retryCount = 0;
-            const maxRetries = 10000; // Define the maximum number of retries
-
-            while (retryCount < maxRetries) {
-                try {
-                    await processPage(currentHref); // Increase the timeout to 30 seconds
-                    break; // If successful, break out of the loop
-                } catch (error) {
-                    if (error.name === 'TimeoutError') {
-                        console.error(
-                            `Timeout occurred (Retry ${retryCount + 1}/${maxRetries}). Retrying...`
-                        );
-                        retryCount++;
-                    } else {
-                        throw error; // Rethrow other errors
-                    }
-                }
-            }
-
-            if (retryCount >= maxRetries) {
-                console.error(
-                    `Max retries (${maxRetries}) reached. Unable to load the page: ${currentHref}`
-                );
-                // Handle the situation when the page can't be loaded after maximum retries.
-                await pageForEvaluation.close();
-                continue; // Move on to the next URL
-            }
-
-            await pageForEvaluation.close();
-        }
-    } catch (error) {
-        console.error('An error occurred:', error);
-    } finally {
-        if (browser) {
-            await browser.close(); // Close the browser when done
-        }
-        console.log('final')
-    }
     
 }
+createBrowser().then ( () => {
+    main()
+}).catch(() => {
+    console.log('faileeeeed')
+})
 
-main()
 
