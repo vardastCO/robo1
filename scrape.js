b4946f6400305c928b59be19daba29fcfd529f50
partovi <partovi74@gmail.com>
2023-10-05 06:53:18 -0700

    cahnge seed

--- scrape.js
+++ scrape.js
@@ -17,25 +17,25 @@ const startUrlPattern = 'https://www.hypersaz.com/product.php?';
     let browser; // Declare the browser variable
         const proxyServer =
             'ss://YWVzLTI1Ni1nY206d0DVaGt6WGpjRA==@38.54.13.15:31214#main';
-        try {
-            browser = await puppeteer.launch({
-                headless: true, // Set to true for headless mode, false for non-headless
-                executablePath:  process.env.NODE_ENV === "production" ?
-                  process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
-                args: [
-                    '--no-sandbox',
-                    `--proxy-server=${proxyServer}`,
-                    '--disable-setuid-sandbox',
-                    '--enable-logging',
-                    '--no-zygote',
-                    '--single-process'
-                ],
-            });
+        // try {
+        //     browser = await puppeteer.launch({
+        //         headless: true, // Set to true for headless mode, false for non-headless
+        //         executablePath:  process.env.NODE_ENV === "production" ?
+        //           process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+        //         args: [
+        //             '--no-sandbox',
+        //             `--proxy-server=${proxyServer}`,
+        //             '--disable-setuid-sandbox',
+        //             '--enable-logging',
+        //             '--no-zygote',
+        //             '--single-process'
+        //         ],
+        //     });
 
 
-        } catch (error) {
-            console.error('Error while launching the browser:', error);
-        }
+        // } catch (error) {
+        //     console.error('Error while launching the browser:', error);
+        // }
 
 
     const processedHrefs = new Set();
@@ -44,46 +44,60 @@ const startUrlPattern = 'https://www.hypersaz.com/product.php?';
     async function processPage(pageUrl) {
         console.log('Processing Page:', pageUrl);
         try {
+            browser = await puppeteer.launch({
+                        headless: true, // Set to true for headless mode, false for non-headless
+                        executablePath:  process.env.NODE_ENV === "production" ?
+                          process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+                        args: [
+                            '--no-sandbox',
+                            `--proxy-server=${proxyServer}`,
+                            '--disable-setuid-sandbox',
+                            '--enable-logging',
+                            '--no-zygote',
+                            '--single-process'
+                        ],
+                    });
             const page = await browser.newPage();
             await page.goto(pageUrl, { timeout: 120000 });
             await page.screenshot();
+            const priceElement = await page.$x(
+                '/html/body/section[2]/div/div/div[3]/div/ul/li[2]/p/span'
+            );
+            if (priceElement.length > 0) {
+                const priceText = await page.evaluate(
+                    (el) => el.textContent,
+                    priceElement[0]
+                );
+                if (priceText.trim() !== '') {
+                    const record = [{ url: pageUrl, price: priceText.trim() }];
+                    await csvWriter.writeRecords(record);
+                    console.log(`Saved: URL: ${pageUrl}, Price: ${priceText.trim()}`);
+                }
+            }
+            const hrefs = await page.evaluate(() => {
+                const links = Array.from(document.querySelectorAll('a'));
+                return links.map((link) => link.getAttribute('href'));
+            });
+    
+            for (const href of hrefs) {
+                if (href && !processedHrefs.has(href)) {
+                    if (!href.startsWith('https://')) {
+                        var outputUrl = initialPage + href;
+                    } else {
+                        var outputUrl = href;
+                    }
+                    if (outputUrl.startsWith(startUrlPattern)) {
+                        unprocessedHrefs.add(outputUrl);
+                    }
+                }
+            }
+            await page.close();
           } catch (error) {
             console.error('An error occurred while navigating to the page farbooood:', error);
             // Handle the error as needed
           }
 
-        const priceElement = await page.$x(
-            '/html/body/section[2]/div/div/div[3]/div/ul/li[2]/p/span'
-        );
-        if (priceElement.length > 0) {
-            const priceText = await page.evaluate(
-                (el) => el.textContent,
-                priceElement[0]
-            );
-            if (priceText.trim() !== '') {
-                const record = [{ url: pageUrl, price: priceText.trim() }];
-                await csvWriter.writeRecords(record);
-                console.log(`Saved: URL: ${pageUrl}, Price: ${priceText.trim()}`);
-            }
-        }
-        const hrefs = await page.evaluate(() => {
-            const links = Array.from(document.querySelectorAll('a'));
-            return links.map((link) => link.getAttribute('href'));
-        });
-
-        for (const href of hrefs) {
-            if (href && !processedHrefs.has(href)) {
-                if (!href.startsWith('https://')) {
-                    var outputUrl = initialPage + href;
-                } else {
-                    var outputUrl = href;
-                }
-                if (outputUrl.startsWith(startUrlPattern)) {
-                    unprocessedHrefs.add(outputUrl);
-                }
-            }
-        }
-        await page.close();
+        
     }
 
     try {
